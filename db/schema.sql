81d3e0e59a16ce49f161213cf2bcc960c3748e4b
Holly Parker <hollygcochran@gmail.com>
2022-10-15 14:23:12 -0500

    EMPLOYEE TABLE SCHEMA

--- db/schema.sql
+++ db/schema.sql
@@ -13,3 +13,14 @@ CREATE TABLE role (
     name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT NULL,
+
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
